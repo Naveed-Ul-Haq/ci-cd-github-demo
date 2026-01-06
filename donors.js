368812a09a3f3e9eb4d504a924950a9acf75cb71
Naveed Ul Haq <naveedulhaq75@gmail.com>
2026-01-06 14:24:10 +0500

    Add public HTML and live donor table

---
+++ donors.js
@@ -0,0 +1,8 @@
+// donors.js
+const donors = [
+  { id: 1, name: "Ali", bloodType: "A+" },
+  { id: 2, name: "Sara", bloodType: "B-" },
+  { id: 3, name: "Omar", bloodType: "AB+" },
+];
+
+module.exports = donors;
--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const donors = require('./donors');
 const app = express();
 
+// Serve static HTML files
+app.use(express.static('public'));
+
+// Health check
 app.get('/health', (req, res) => {
   res.send('App is running');
+});
+
+// Show all donors
+app.get('/donors', (req, res) => {
+  console.log("Donors endpoint called"); // log to console
+  res.json(donors);
 });
 
 app.listen(3000, () => {
