b6d2346158572d2a250a58d3bf9591c3c1233bde
Naveed Ul Haq <148003986+Naveed-Ul-Haq@users.noreply.github.com>
2026-01-08 17:34:34 +0500

    Add new donor 'Asim' with blood type 'B+'

--- donors.js
+++ donors.js
@@ -4,6 +4,7 @@ const donors = [
   { id: 2, name: "Sara", bloodType: "B-" },
   { id: 3, name: "Omar", bloodType: "AB+" },
   { id: 4, name: "Sohaib", bloodType: "O+"},
+  { id: 5, name: "Asim", bloodType: "B+"},
 ];
 
 module.exports = donors;
