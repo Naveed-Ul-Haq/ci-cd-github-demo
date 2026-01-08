286e3676b2663d11634518f16edb3b8cea0e4d9a
Naveed Ul Haq <naveedulhaq75@gmail.com>
2026-01-08 22:40:01 +0500

    deleted 2 donors

--- donors.js
+++ donors.js
@@ -6,9 +6,7 @@ const donors = [
   { id: 3, name: "Omar", bloodType: "AB+" },
   { id: 4, name: "Sohaib", bloodType: "O+"},
   { id: 5, name: "Asim", bloodType: "B+"},
-  { id: 6, name: "Lina", bloodType: "A-"},
-  { id: 7, name: "Nora", bloodType: "O-"},
-  { id: 8, name: "Khalid", bloodType: "AB-"},
+
 
 
 
