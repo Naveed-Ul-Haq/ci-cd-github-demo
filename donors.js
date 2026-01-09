f087bc6f3e0614ae040e9c2d3e47784066bbc12d
Naveed Ul Haq <148003986+Naveed-Ul-Haq@users.noreply.github.com>
2026-01-09 11:30:15 +0500

    Remove Sara from donors list

--- donors.js
+++ donors.js
@@ -2,7 +2,6 @@
 // donors.js
 const donors = [
   { id: 1, name: "Ali", bloodType: "A+" },
-  { id: 2, name: "Sara", bloodType: "B-" },
   { id: 3, name: "Omar", bloodType: "AB+" },
   { id: 4, name: "Sohaib", bloodType: "O+"},
   { id: 5, name: "Asim", bloodType: "B+"},
